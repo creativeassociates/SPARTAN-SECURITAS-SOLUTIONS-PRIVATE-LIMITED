eccb879bcaa6fb447362a79a6fad449d2c01aacf
StagerCoder <rohankakkar1997@gmail.com>
2024-10-03 23:51:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="maintenance-container">
+            <div className="maintenance-content">
+                <h2 className="company-heading animated-heading">SPARTAN SECURITAS SOLUTIONS PRIVATE LIMITED</h2>
+                <h1 className="animated-message">🚧 Maintenance in Progress 🚧</h1>
+                <p className="animated-text">
+                    We’re currently working on some improvements! <br />
+                    Please check back soon.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default App;
